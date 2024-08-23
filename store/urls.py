47fcadb86fd9da11ef1463c0ad9db677372a2b5d
sujoncep <sujoncep@gmail.com>
2024-08-23 08:49:39 +0600

    1.template integrated with dajngo 2. authentication implemented

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('blog', views.blog, name='blog'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html',{})
+    products = Product.objects.all()
+    return render(request, 'home.html',{'products':products})
+
+def about(request):
+    return render(request, 'about.html',{})
+
+
+
+def contact(request):
+    return render(request, 'contact.html',{})
+
+def blog(request):
+    return render(request, 'blog.html',{})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have successfully logged in!"))
+            return redirect('home')
+        else:
+            messages.succes(request, ("There is an error!"))
+            return redirect('login')
+        
+    else:
+        return render(request, 'login.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have successfully logged out!"))
+    return redirect('home')
