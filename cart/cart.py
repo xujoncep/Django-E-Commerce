0f4141a8d2e384b34b464e90a86523225e7d4098
sujoncep <sujoncep@gmail.com>
2024-08-28 03:53:07 +0600

    Quantity fixed

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+from store.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -12,18 +13,33 @@ class Cart():
         #make sure cart is avilable on all the page of site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         #logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
         
         self.session.modified = True
     
 
     def __len__(self):
         return len(self.cart)
+    
+    def get_prods(self):
+        #get id from cart
+        product_ids = self.cart.keys()
+        #use id lookup product in dstsbsde moddel
+        products = Product.objects.filter(id__in=product_ids)
+
+        return products
+    
+    def get_quantity(self):
+        quantities = self.cart
+        return quantities
+
 
     
--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,10 @@ from django.http import JsonResponse
 
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html',{})
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quantity
+    return render(request, 'cart_summary.html',{'cart_products':cart_products, 'quantities':quantities})
 
 def cart_add(request):
     #get the cart
@@ -14,10 +17,12 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         #get stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
         # product look into db
         product = get_object_or_404(Product, id=product_id)
         #save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         #get cart quantity
         cart_quantity = cart.__len__()
         #return response
